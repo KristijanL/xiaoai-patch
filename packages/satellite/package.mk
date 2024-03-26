d092166c1abd967e0af07a57adcb73a6e32a3548
Kristijan Lecnik <kristijan@l-tech.systems>
2024-03-26 19:23:28 +0100

    update wyoming-satellite package

--- packages/satellite/package.mk
+++ packages/satellite/package.mk
@@ -1,15 +1,26 @@
 PACKAGE_NAME="satellite"
-PACKAGE_VERSION="1.1.0"
+PACKAGE_VERSION="1.2.0"
 PACKAGE_SRC="https://github.com/rhasspy/wyoming-satellite/archive/v${PACKAGE_VERSION}.tar.gz"
 PACAKGE_DEPENDS="python3 aec-cmdline"
 
-make_package() {
-	# TODO
-	return 0
+preconfigure_package() {
+	FILE="v${PACKAGE_VERSION}.tar.gz"
+	if [ ! -e "${PACKAGE_SRC_DOWNLOAD_DIR}/${FILE}" ]; then
+		echo_info "downloading wyoming-satellite - ${FILE}"
+                wget -O ${PACKAGE_SRC_DOWNLOAD_DIR}/${FILE} "${PACKAGE_SRC}"
+	fi
 }
 
 install_package() {
+	mkdir -p ${STAGING_DIR}/usr/share/satellite/
 
+	cp -rv ${PACKAGE_SRC_DIR}/sounds ${PACKAGE_SRC_DIR}/wyoming_satellite ${PACKAGE_SRC_DIR}/script ${STAGING_DIR}/usr/share/satellite/
+
+	cp -rv ${PACKAGE_DIR}/config/venv ${STAGING_DIR}/usr/share/satellite/.venv
+	cp -v ${PACKAGE_DIR}/config/launcher ${STAGING_DIR}/usr/share/satellite/launcher
+}
+
+postinstall_package() {
 	cp -v ${PACKAGE_DIR}/config/satellite.init ${STAGING_DIR}/etc/init.d/listener
 	ln -sf ../init.d/listener ${STAGING_DIR}/etc/rc.d/S98listener
 
